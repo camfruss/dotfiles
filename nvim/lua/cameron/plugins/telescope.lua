1b425cd3f39acb86120d3cc5e213bb131f2f2f36
Cameron Russell <camfruss@gmail.com>
2026-01-19 23:12:06 -0800

    updates

--- nvim/lua/cameron/plugins/telescope.lua
+++ nvim/lua/cameron/plugins/telescope.lua
@@ -15,9 +15,11 @@ return {
 			local builtin = require('telescope.builtin')
 			vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 			vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, { desc = 'Telescope document symbols' })
-			vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-			vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-			vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+			vim.keymap.set('n', '<leader>lg', builtin.live_grep, { desc = 'Telescope live grep' })
+			vim.keymap.set('n', '<leader>bf', builtin.buffers, { desc = 'Telescope buffers' })
+			vim.keymap.set('n', '<leader>ht', builtin.help_tags, { desc = 'Telescope help tags' })
+			vim.keymap.set('n', '<leader>sh', builtin.search_history, { desc = 'Telescope search history' })
+			vim.keymap.set('n', '<leader>of', builtin.oldfiles, { desc = 'Telescope old files' })
 		end,
 	},
 }
